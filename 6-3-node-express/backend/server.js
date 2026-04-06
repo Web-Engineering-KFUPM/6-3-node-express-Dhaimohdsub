53d10eb6f72250692fda2b8934c5c0719b70180b
Dhaimohdsub <s202270640.kfupm.sa.edu>
2026-04-06 11:47:20 +0300

    final

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -15,7 +15,7 @@
 // TODO 6.2: Create "/api/quote" route
 
 
-// TODO 7: Start server using app.listen
+// TODO 7: Start server using app..listen
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
